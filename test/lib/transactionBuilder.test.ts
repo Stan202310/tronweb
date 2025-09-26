761c788d2780e03c0e0e93a7bcedf85552dc915c
Stan202310 <Stan202310@no-reply.com>
2025-09-26 16:03:39 +0800

    feat: add test for updateAccountPermissions

--- test/lib/transactionBuilder.test.ts
+++ test/lib/transactionBuilder.test.ts
@@ -2946,6 +2946,54 @@ describe('TronWeb.transactionBuilder', function () {
                 await tronWeb.trx.sign(transaction, accounts.pks[6]);
             }
         });
+        it('should accept Permission type of activePermissions', async function () {
+            const permissionData = {
+                owner: {
+                    type: 0,
+                    keys: [
+                        {
+                            address: accounts.hex[6],
+                            weight: 1,
+                        },
+                    ],
+                    threshold: 1,
+                    permission_name: 'owner',
+                },
+                witness: {
+                    keys: [
+                        {
+                            address: accounts.hex[6],
+                            weight: 1,
+                        },
+                    ],
+                    threshold: 1,
+                    id: 1,
+                    type: 1,
+                    permission_name: 'witness',
+                },
+                owner_address: accounts.hex[6],
+                actives: {
+                    operations: '7fff1fc0033e0000000000000000000000000000000000000000000000000000',
+                    keys: [
+                        {
+                            address: accounts.hex[6],
+                            weight: 1,
+                        },
+                    ],
+                    threshold: 1,
+                    id: 2,
+                    type: 2,
+                    permission_name: 'active',
+                },
+            };
+            const tx = await tronWeb.transactionBuilder.updateAccountPermissions(
+                permissionData.owner_address,
+                permissionData.owner,
+                permissionData.witness,
+                permissionData.actives,
+            );
+            assert.isObject(tx);
+        });
     });
 
     describe('Alter existent transactions', async function () {
